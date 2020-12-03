a11ed8eaedbe9471ac24c13aed9a6f1096af850b
RomanK <romulus_666@mail.ru>
2020-12-03 15:43:25 +0600

    Initial commit

---
+++ desktop/src/ru/geekbrains/star_game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.star_game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.star_game.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
