03fd9236050217a222884c9b7cef112c24a7a181
RomanK <romulus_666@mail.ru>
2020-12-07 19:55:38 +0600

    homework2

--- core/src/ru/geekbrains/star_game/StarGame.java
+++ core/src/ru/geekbrains/star_game/StarGame.java
@@ -1,11 +1,6 @@
 package ru.geekbrains.star_game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import ru.geekbrains.star_game.screen.MenuScreen;
 
