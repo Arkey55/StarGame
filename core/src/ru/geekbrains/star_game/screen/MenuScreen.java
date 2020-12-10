78d886e23dfc11e272327cf77865c75d3b3f246e
RomanK <romulus_666@mail.ru>
2020-12-10 12:49:59 +0600

    changed ship movement logic

--- core/src/ru/geekbrains/star_game/screen/MenuScreen.java
+++ core/src/ru/geekbrains/star_game/screen/MenuScreen.java
@@ -1,7 +1,6 @@
 package ru.geekbrains.star_game.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
@@ -12,19 +11,25 @@ public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Texture background;
-    private Vector2 currentPosition;
 
-    private float speed = 1f;
+    private static final float speed = 2f;
     private int screenX;
     private int screenY;
-    Vector2 target;
-    Vector2 direction;
 
-    private void moveShipToMousePosition (){
-        target = new Vector2(screenX, screenY);
-        direction = new Vector2(target.sub(currentPosition)).nor();
-        currentPosition.add(direction.scl(speed));
-        //не знаю как остановить, продолжает вибрировать в точке назначения
+    private Vector2 pos;
+    private Vector2 target;
+    private Vector2 velocity;
+    private Vector2 temp;
+
+    private void moveShip(){
+        target.set(screenX, screenY);
+        velocity.set(target.cpy().sub(pos).setLength(speed));
+        temp.set(target);
+        if (temp.sub(pos).len() > velocity.len()){
+            pos.add(velocity);
+        } else {
+            pos.set(target);
+        }
     }
 
     @Override
@@ -32,18 +37,21 @@ public class MenuScreen extends BaseScreen {
         super.show();
         img = new Texture("spaceship2.png");
         background = new Texture("kosmos.jpg");
-        currentPosition = new Vector2();
+        pos = new Vector2();
+        target = new Vector2();
+        velocity = new Vector2();
+        temp = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        moveShip();
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(img, currentPosition.x, currentPosition.y, 128, 128);
-        moveShipToMousePosition();
+        batch.draw(img, pos.x, pos.y, 128, 128);
         batch.end();
     }
 
@@ -58,24 +66,12 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         this.screenX = screenX - 64;
         this.screenY = Gdx.graphics.getHeight() - screenY - 64;
-        return super.touchDown(screenX, screenY, pointer, button);
+        return false;
     }
+
+
     @Override
     public boolean keyDown(int keycode) {
-        switch (keycode){
-            case Input.Keys.W:
-                currentPosition.y += 10;
-                break;
-            case Input.Keys.S:
-                currentPosition.y -= 10;
-                break;
-            case Input.Keys.A:
-                currentPosition.x -= 10;
-                break;
-            case Input.Keys.D:
-                currentPosition.x += 10;
-                break;
-        }
-        return super.keyDown(keycode);
+        return false;
     }
 }
